4fe683192cfd341bcd35b4570602d17bc4cfdc17
Dhairya Mehta <72181239+DhairyaMeh@users.noreply.github.com>
2021-10-16 13:37:12 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,29 +1,36 @@
-#include <studio.h>
-
-
+#include<stdio.h>
+#define MAXSIZE 50
+void bubble(int x[], int n)
+{
+	int i,j,temp;
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n-1;j++)
+		{
+			if(x[j] > x[j+1])
+			{
+				temp = x[j];
+				x[j] = x[j+1];
+				x[j+1] = temp;
+			}
+		}
+	}
+}
 int main()
 {
-    bool swap;
-    int n=5,j;
-    int arr[5]={58,89,71,35,6};
-    
-    
-    do{
-        swap=false;
-        for(j=0;j<n;j++)
-        {
-            if (arr[j]>arr[j-1]);
-            {
-                temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp
-                swap=false;
-            }
-        }
-    }while(swap);
-    
-    for(m=0;m<n;m--)
-        {
-            printf("%s ",arr[l]);
-        }
+	int x[MAXSIZE],n,i;
+	printf("enter the number of elements");
+	scanf("%d",&n);
+	printf("\n Enter the elements:");
+	for(i=0; i<n; i++)
+	{
+		scanf("%d",&x[i]);
+	}
+	bubble(x,n);
+	printf("\n The sorted output:\n");
+	for(i=0;i<n;i++)
+	{
+		printf("\n%d",x[i]);
+	}
+	return 0;
 }
